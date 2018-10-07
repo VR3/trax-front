7b16be798175a15ebe47b81f77f59dca36028455
oschvr <oscarchavezromero@gmail.com>
2018-10-07 03:08:45 -0500

    Add dynamic visualization for global amount

--- src/App.js
+++ src/App.js
@@ -20,14 +20,18 @@ class App extends Component {
           <header className="App-header">
             <h1 style={{marginTop: '-15vh', color: 'purple'}}>Teleton</h1>
             <h2 style={{marginTop: '-30px'}}>Trax</h2>
-              <div class="lds-circle" style={{zIndex: 10}}></div>
-              <img src={pool1} style={{position: 'absolute', bottom: '195px', width:"100px"}} />
-              <img src={pool2} style={{position: 'absolute',bottom: '7px', width:"100px", zIndex: 12}} />
+              <div className="lds-circle" style={{zIndex: 10}}></div>
+              <div className="wrapper">
+                <div className="expected"></div>
+                <div className="collected-bg"></div>
+                <div className="collected"></div>
+                <div className="wires"></div>
+              </div>
           </header>
         </section>
         <section id="section02">
           <header className="App-header2">
-            <div class="lds-circle" style={{zIndex: 1, position: 'absolute', top: '0', marginTop: '-10vh'}}></div>
+            <div className="lds-circle" style={{zIndex: 1, position: 'absolute', top: '0', marginTop: '-10vh'}}></div>
             <img src={Whiteline} style={{height: '100vh'}} />
           </header>
         </section>
